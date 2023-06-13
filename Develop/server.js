f289c4e89cf565731fc8c691ec8d8632a6d47243
Douped <anthony.rooyakkers2001@gmail.com>
2023-06-13 02:09:39 -0400

    configured express and api routes in server.js

---
+++ Develop/server.js
@@ -0,0 +1,18 @@
+// import modules
+const express = require("express");
+const routes = require("./routes");
+
+// create express application
+const app = express();
+const PORT = process.env.PORT || 3001;
+// middleware for parsing JSON
+app.use(express.json());
+// middleware for url-encoded form data
+app.use(express.urlencoded({ extended: true }));
+// middleware for public directory
+app.use(express.static("public"));
+
+app.use(routes);
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
